fd4e8c7b658f801bba59712da33296ad7302a94f
Larson-Dav <larsonkapita@gmail.com>
2023-09-28 12:01:30 +0200

    Adding Tables DataREsources

--- index.js
+++ index.js
@@ -3,3 +3,64 @@ console.log('Random Message generator')
 console.log('Let started')
 
 console.log('On develop')
+
+const dataRource1 = [
+    {
+      title: 'The Road Ahead',
+      author: 'Bill Gates',
+      libraryID: 1954
+    },
+    {
+      title: 'Walter Isaacson',
+      author: 'Steve Jobs',
+      libraryID: 7389
+    },
+    {
+      title: 'Mockingjay: The Final Book of The Hunger Games',
+      author: 'Suzanne Collins',
+      libraryID: 1245
+    }
+]
+
+const dataRource2 = [
+    {
+      app: 'Wikipedia',
+      user: '+10M',
+      libraryID: 1922
+    },
+    {
+      title: 'Google',
+      author: 'Over 4 billion',
+      libraryID: 4264
+    },
+    {
+      title: 'Facebook',
+      user: '2 billion',
+      created: 1945
+    }
+]
+
+const dataRource = [
+    {
+      title: 'christof',
+      age: 12,
+      passion: 'developement'
+    },
+    {
+        title: 'john',
+        age: 56,
+        passion: 'developement'
+    },
+    {
+        title: 'christof',
+        age: 12,
+        passion: 'developement'
+    }
+]
+
+const greet = ['Bonjour','Bonsoir','Salue','Hey']
+
+
+
+
+
