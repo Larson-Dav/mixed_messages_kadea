81cea1b6321762237d058aed98f105a02517f0a8
Larson-Dav <larsonkapita@gmail.com>
2023-09-29 10:12:49 +0200

    I reoganize to code to final Use

--- index.js
+++ index.js
@@ -1,77 +1,66 @@
-// console.log('Random Message generator')
-
-// console.log('Let started')
-
-// console.log('On develop')
-
 const dataRource1 = [
-    {
-      1: 'The Road Ahead',
-      2: 'Bill Gates',
-      3: 1954
-    },
-    {
-      1: 'Walter Isaacson',
-      2: 'Steve Jobs',
-      3: 7389
-    },
-    {
-      1: 'Mockingjay: The Final Book of The Hunger Games',
-      2: 'Suzanne Collins',
-      3: 1245
-    }
+    {book: '-Silicon valley-',author: 'Bill Gates',pages: 5543},
+    {book: '-The Giant Story-',author: 'Steve Jobs',pages: 450},
+    {book: '-Mockingjay: The Final Book of The Hunger Games-',author: 'Suzanne Collins',3: 1000},
+    {book: '-Dev Strory-',author: 'Natan  Spenser',pages: 1245},
+    {book: '-Quantum Innovation-',author: 'Dawn jonh',pages: 1245},
+    {book: '-Loggin into dream-',author: 'Steve Hunter',pages: 1245},
+    {book: '-From Nightmare to Light-',author: 'Harvey  Specter',pages: 3409},
+    {book: '-Last Code-',author: 'Suzanne Collins',pages: 4590},
+    {book: '-Servers-',author: 'Rey james',pages: 300},
+    {book: '-Little Parametre-',author: 'Gounter  Collins',pages: 837}
 ]
 
 const dataRource2 = [
-    {
-      1: 'Wikipedia',
-      2: '+10M',
-      3: 1922
-    },
-    {
-      1: 'Google',
-      2: 'Over 4 billion',
-      3: 4264
-    },
-    {
-      1: 'Facebook',
-      2: '2 billion',
-      3: 1945
-    }
+    {Enterprise: 'Wikipedia',users: '+10M',Year: 1922},
+    {Enterprise: 'Google',users: 'plus de 4 milliards',Year: 2004},
+    {Enterprise: 'Facebook',users: '+30M',Year: 2011},
+    {Enterprise: 'Pinterest',users: '+45M',Year: 1967},
+    {Enterprise: 'Codingame',users: '+500M',Year: 1890},
+    {Enterprise: 'GoInnovation',users: '+4M',Year: 1998},
+    {Enterprise: 'Microsoft',users: '+23M',Year: 2015},
+    {Enterprise: 'Amazon',users: '+33M',Year: 1945},
+    {Enterprise: 'Alibaba',users: 'plus 64 milliards',Year: 2017},
+    {Enterprise: 'Tesla',users: 'plus de 10 milliards',Year: 2003}
 ]
 
 const dataRource = [
-    {
-      1: 'christof',
-      2: 12,
-      3: 'developement'
-    },
-    {
-        1: 'john',
-        2: 56,
-        3: 'developement'
-    },
-    {
-        1: 'christof',
-        2: 12,
-        3: 'developement'
-    }
+    {nom: 'Christof',age: 12,passion: 'Programmation'},
+    {nom: 'John',age: 56,passion: 'Developement Web'},
+    {nom: 'Christof',age: 12,passion: 'linnovation'},
+    {nom: 'Emma',age: 12,passion: 'Ux Design'},
+    {nom: 'James',age: 12,passion: 'Dev Mobile'},
+    {nom: 'Noah',age: 12,passion: 'Informatique'},
+    {nom: 'Amelia',age: 12,passion: 'Entreprenariat'},
+    {nom: 'Olivia',age: 12,passion: 'Engenierie'},
+    {nom: 'Geeks',age: 12,passion: 'Programmation Backend'},
+    {nom: 'Thierry',age: 12,passion: 'Prog Frontend'}
 ]
 
 const greet = ['Bonjour','Bonsoir','Salue','Hey','salam','Hi','Hello','Holla','yo','plesea']
-const cname = ['steve jobs','Larry page','Mark zukeberg','john smith','linus makfield']
+const cname = ['Steve Jobs','Larry Page','Mark Zukeberg','Elon Musk','Jack Ma',
+               'Xian Bing','Bill Gate','Curl yung','Liong Ben','Linus Torvalds']
 
 
 
 function MessageGenerator(){
-  const mess = greet[parseInt(Math.random()*10)]
-  const noms = dataRource[parseInt(Math.random()*2)][1]
-  const ent_name =  dataRource2[parseInt(Math.random()*2)][1]
-  const ent_date =  dataRource2[parseInt(Math.random()*2)][3]
-  const ent_user =  dataRource2[parseInt(Math.random()*2)][2]
-  const ent_creator = cname[parseInt(Math.random()*3)]
+  const random = Math.random()*9
+  const mess = greet[parseInt(random)]
+  const noms = dataRource[parseInt(random)]['nom']
+  const ent_name =  dataRource2[parseInt(random)]['Enterprise']
+  const ent_date =  dataRource2[parseInt(random)]['Year']
+  const ent_user =  dataRource2[parseInt(random)]['users']
+  const ent_creator = cname[parseInt(random)]
+  const pass = dataRource[parseInt(random)]['passion']
+  const book = dataRource1[parseInt(random)]['book']
+  const author = dataRource1[parseInt(random)]['author']
+  const pages = dataRource1[parseInt(random)]['pages']
   
-  return mess + ' ' + noms + ', ' + ent_name + ' a ete creer en ' + ent_date + ' par ' + ent_creator
+  return mess + " "  + noms + ", " + ent_name + " a été créée en " + 
+         ent_date + " par " + ent_creator + " et compte aujourd'hui " + 
+         ent_user + " d'utilisateurs . Il a été conçu par des passionnés de|d' " + 
+         pass + ". Vous retrouverez toute l'histoire dans le livre "+
+         book + " de " + author + " qui est à moins "+ pages + " Pages."
 }
 console.log(MessageGenerator())
 
